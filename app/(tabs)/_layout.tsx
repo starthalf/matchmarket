646a38540376125838c6096077eedbffb8e7e5a3
starthalf <hcgkhlee@gmail.com>
2025-09-18 17:50:16 +0900

    Updated _layout.tsx

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,5 +1,5 @@
 import { Tabs } from 'expo-router';
-import { Home, Plus, User, MessageCircle, ClipboardList } from 'lucide-react-native';
+import { Chrome as Home, Plus, User, MessageCircle, ClipboardList } from 'lucide-react-native';
 
 export default function TabLayout() {
   return (

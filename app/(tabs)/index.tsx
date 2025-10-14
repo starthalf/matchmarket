7c31c8d5e9218bb549df345a465f665e1d2feee9
starthalf <hcgkhlee@gmail.com>
2025-10-14 14:46:55 +0900

    Updated index.tsx

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -615,7 +615,7 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     alignItems: 'center',
     paddingHorizontal: 16,
-    paddingVertical: 12,
+    paddingVertical: 10,
     backgroundColor: '#eeeeee',
     gap: 12,
   },

2e2cf4526e3340dc3a5d6e08360141948f5c2f90
starthalf <hcgkhlee@gmail.com>
2025-12-15 12:40:54 +0900

    Updated service-worker.js

---
+++ public/service-worker.js
@@ -0,0 +1,47 @@
+// public/service-worker.js
+const CACHE_NAME = 'matchmarket-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+];
+
+// 설치 이벤트
+self.addEventListener('install', (event) => {
+  console.log('Service Worker: Installing...');
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Service Worker: Caching files');
+        return cache.addAll(urlsToCache);
+      })
+      .then(() => self.skipWaiting())
+  );
+});
+
+// 활성화 이벤트
+self.addEventListener('activate', (event) => {
+  console.log('Service Worker: Activating...');
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Service Worker: Deleting old cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  return self.clients.claim();
+});
+
+// Fetch 이벤트 (네트워크 우선 전략)
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request)
+      .catch(() => {
+        return caches.match(event.request);
+      })
+  );
+});

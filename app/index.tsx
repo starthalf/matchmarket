aa2f1010792ac73a56c926d8815fb5633a4f654b
starthalf <hcgkhlee@gmail.com>
2025-12-19 16:15:47 +0900

    Updated index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -41,7 +41,7 @@ export default function Index() {
         return prev + Math.floor(Math.random() * 4 + 1) * 500;
       });
 
-    }, 80); // 0.08초마다 갱신
+    }, 80); // 0.05초마다 갱신
 
     return () => clearInterval(interval);
   }, [isAnimating]);

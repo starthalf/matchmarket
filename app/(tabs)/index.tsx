0ebd9e08f4b48594d30f2a78cb0a4dd73a210667
starthalf <hcgkhlee@gmail.com>
2026-04-23 17:51:23 +0900

    Updated index.tsx

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,5 @@
 // app/(tabs)/index.tsx
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState, useRef, useEffect, useMemo } from 'react';
 import {
   View,
   Text,

15e80fb35c1cb9997c5412f2902b670bf8f395f0
starthalf <hcgkhlee@gmail.com>
2026-07-20 15:49:22 +0900

    Updated earnings.tsx

--- app/(tabs)/earnings.tsx
+++ app/(tabs)/earnings.tsx
@@ -18,7 +18,6 @@ import { getMockEarnings, EarningsData } from '../../data/mockData';
 import { useSafeStyles } from '../../constants/Styles';
 import { EarningsManager, MonthlySettlement } from '../../utils/earningsManager';
 import { PopularityGauge } from '../../components/PopularityGauge';
-{currentUser && <PopularityGauge sellerId={currentUser.id} />}
 
 export default function EarningsScreen() {
   const { user: currentUser } = useAuth();
